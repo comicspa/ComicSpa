b39d545217546121ca5b9c3fe8e0c1aa1a3300da
SangYeon <sam@superants.io>
2019-08-01 16:00:47 +0900

    - adding custom font and updating pubspec - implementing Card for Trend and Trend content titles

--- first_ui/lib/screens/Trend.dart
+++ first_ui/lib/screens/Trend.dart
@@ -34,40 +34,52 @@ class _TrendState extends State<Trend> {
     return SingleChildScrollView(
       child: Column(
         mainAxisSize: MainAxisSize.min,
+
         children: <Widget>[
-          Padding(
-            padding: EdgeInsets.all(0.0),
-            child: FutureBuilder<List<TodayPopularComicInfo>>(
-              future: c2STodayPopularComicInfo.fetchBytes(),
-              builder: (context,snapshot){
-                if(!snapshot.hasData) return CircularProgressIndicator();
-                {
-                  return CarouselSlider(
-                      autoPlay: false,
-                      height: 250.0,
-                      aspectRatio: 16/9,
-                      initialPage: 0,
-                      viewportFraction: 1.0,
-                      enableInfiniteScroll: true,
-                      scrollDirection: Axis.horizontal,
+          SizedBox(
+            height: 250,
+            child: Padding(
+              padding: EdgeInsets.all(0.0),
+              child: FutureBuilder<List<TodayPopularComicInfo>>(
+                future: c2STodayPopularComicInfo.fetchBytes(),
+                builder: (context,snapshot){
+                  if(!snapshot.hasData) return Center(child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      SizedBox(
+                        height: 25,
+                        width: 25,
+                        child: CircularProgressIndicator()),
+                    ],
+                  ));
+                  {
+                    return CarouselSlider(
+                        autoPlay: false,
+                        height: 250.0,
+                        aspectRatio: 16/9,
+                        initialPage: 0,
+                        viewportFraction: 1.0,
+                        enableInfiniteScroll: true,
+                        scrollDirection: Axis.horizontal,
 
 
-                      items:snapshot.data.map((i){
-                        return Builder (
-                            builder: (BuildContext context){
-                              return Container(
+                        items:snapshot.data.map((i){
+                          return Builder (
+                              builder: (BuildContext context){
+                                return Container(
 
-                                width: MediaQuery.of(context).size.width,
-                                margin: EdgeInsets.symmetric(horizontal: 0.0),
-                                decoration: BoxDecoration(color: Colors.white),
-                                child: Image.network(i.thumbnailUrl, fit:BoxFit.fill),
-                              );
-                            }
-                        );
-                      }).toList()
-                  );
-                }
-              },
+                                  width: MediaQuery.of(context).size.width,
+                                  margin: EdgeInsets.symmetric(horizontal: 0.0),
+                                  decoration: BoxDecoration(color: Colors.white),
+                                  child: Image.network(i.thumbnailUrl, fit:BoxFit.fill),
+                                );
+                              }
+                          );
+                        }).toList()
+                    );
+                  }
+                },
+              ),
             ),
           ),
 
@@ -80,43 +92,148 @@ class _TrendState extends State<Trend> {
             textAlign: TextAlign.left,
           ),),
           // buildColumn(context),
-          SizedBox(
-            height: 250,
-            child: Card(
-              clipBehavior: Clip.antiAlias,
-              child: InkWell(
-                highlightColor: Colors.transparent,
-                onTap: (){
-                  print('Card was tapped');
+          Stack( //Todo Need to make listview with futurebuilder up to 18 items
+            children: <Widget>[
+              SizedBox(
+                height: 180,
+                width: 200,
+                child: Card(
+                  clipBehavior: Clip.antiAlias,
+                  child: Column(
+                    children: <Widget>[
+                      Container(
+                        height: 90,
+                        width: 200,
+                        child: ClipRRect(
+                            borderRadius: BorderRadius.circular(2.0),
+                            child: Image.asset('images/야옹이.png', fit: BoxFit.fill)),
 
-                },
-                child: Column(
-                  children: <Widget>[
-                    AspectRatio(
-                      aspectRatio: 16/9,
-                      child: Ink.image(
-                        image: AssetImage('images/야옹이.png'),
-                        fit: BoxFit.fill,
                       ),
-                    ),
-                    Expanded(
-                      child: Padding(
-                        padding: EdgeInsets.all(1.0),
-                        child: Column(
-                          children: <Widget>[
-                            Text('title'),
-                            SizedBox(),   // used for spacing purpose
-                            Text('More description'),
-                          ],
+//                    AspectRatio(
+//                      aspectRatio: 16/9,
+//                      child: Ink.image(
+//                        image: AssetImage('images/야옹이.png'),
+//                        fit: BoxFit.fill,
+//                      ),
+//                    ),
+                      Expanded(
+                        child: Padding(
+                          padding: EdgeInsets.all(5.0),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: <Widget>[
+
+                              Text('Title of the contents',
+                                maxLines: 2,
+                                overflow: TextOverflow.ellipsis,
+                                textAlign: TextAlign.left,
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 13,
+                                ),
+                              ),
+                              SizedBox(height: 10,),   // used for spacing purpose
+                              Expanded(child:
+                                Text('More description of title with some catch phrase, more text, longer text, and too many of them',
+                                  maxLines: 2,
+                                  overflow: TextOverflow.ellipsis,
+
+                                  style: TextStyle(
+                                    color: Colors.grey[800],
+                                    fontSize: 11,
+                                  ),
+                                )
+                              ),
+                              Row(
+
+                                children: <Widget>[
+                                  SizedBox(
+                                    width: 90,
+                                      child: Text('ID',
+                                        style: TextStyle(
+                                          color: Colors.blueGrey,
+                                          fontSize: 11,
+                                          fontWeight: FontWeight.bold,
+                                        ),
+                                      ),
+                                  ),
+                                  SizedBox(
+
+                                    width: 90,
+
+
+                                      child: Text('Views: 489,584 abded ididi',
+                                        maxLines: 1,
+                                        overflow: TextOverflow.ellipsis,
+                                        textAlign: TextAlign.right,
+                                        style: TextStyle(
+                                          color: Colors.grey[700],
+                                          fontSize: 11,
+                                        ),
+                                      ),
+
+                                  ),
+                                ],
+                              )
+                            ],
+                          )
                         )
                       )
-                    )
-                  ],
+                    ],
+                  ),
                 ),
-
               ),
+              Positioned.fill(
+                child: Material(
+                  color: Colors.transparent,
+                  child: InkWell(
+
+                    highlightColor: Colors.transparent,
+                    onTap: (){
+                      print('Card was tapped');
+
+                    },),
+                ),
+              ),
+            ],
+          ),
+          Container(
+            alignment: Alignment.centerLeft,
+            padding: EdgeInsets.fromLTRB(15, 20, 0, 5),
+            child: Text(
+              'Realtime Trend',
+              style: TextStyle(fontSize: 18),
+              textAlign: TextAlign.left,
             ),
           ),
+          Container(
+            alignment: Alignment.centerLeft,
+            padding: EdgeInsets.fromLTRB(15, 20, 0, 5),
+            child: Text(
+              'New',
+              style: TextStyle(fontSize: 18),
+              textAlign: TextAlign.left,
+            ),
+          ),
+          Container(
+            alignment: Alignment.centerLeft,
+            padding: EdgeInsets.fromLTRB(15, 20, 0, 5),
+            child: Text(
+              'Today\'s Trend',
+              style: TextStyle(fontSize: 18),
+              textAlign: TextAlign.left,
+            ),
+          ),
+          Container(
+            alignment: Alignment.centerLeft,
+            padding: EdgeInsets.fromLTRB(15, 20, 0, 5),
+            child: Text(
+              'Weekly Trend',
+              style: TextStyle(fontSize: 18),
+              textAlign: TextAlign.left,
+            ),
+          ),
+
 
          ],
 
@@ -154,7 +271,9 @@ class _TrendState extends State<Trend> {
                           child: Ink.image(
                             image: AssetImage('images/야옹이.png'),
                             fit: BoxFit.cover,
-                            child: Container(),
+                            child: Container(
+                              child: Text('testing'),
+                            ),
                           ),
                         ),
                         Positioned(
