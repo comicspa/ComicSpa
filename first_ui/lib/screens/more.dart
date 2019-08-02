826c17a6affeac72f7c4b0f9e62f307180f2ae61
SangYeon <sam@superants.io>
2019-08-02 10:58:24 +0900

    - Adding testing pages and images for testing Viewer

--- first_ui/lib/screens/more.dart
+++ first_ui/lib/screens/more.dart
@@ -77,7 +77,7 @@ class _MoreScreenState extends State<MoreScreen> {
                 ));
           },
           child: Text(
-            "Image viewer testing",
+            "Image viewer testing!",
           ),
         )
       ],
