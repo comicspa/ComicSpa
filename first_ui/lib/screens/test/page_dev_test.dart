777390d1203b49ce170cdaf89de47a9e3aed715b
changkiyoon <cucum45@gmail.com>
2019-08-19 16:51:58 +0900

    1.add image package

--- first_ui/lib/screens/test/page_dev_test.dart
+++ first_ui/lib/screens/test/page_dev_test.dart
@@ -22,8 +22,11 @@ import 'package:first_ui/manage/manage_firebase_auth.dart';
 import 'package:first_ui/manage/manage_firebase_ml_vision.dart';
 import 'package:first_ui/manage/manage_firebase_storage.dart';
 import 'package:first_ui/manage/manage_paint_canvas.dart';
+import 'package:first_ui/manage/manage_access_token.dart';
+
+
 import 'package:first_ui/models/model_view_comic.dart';
-import 'package:corsac_jwt/corsac_jwt.dart';
+import 'package:image/image.dart';
 
 
 
@@ -66,8 +69,7 @@ class _PageDevTestState extends State<PageDevTest> {
     c2SNewCreatorInfo.generate();
     c2SWeeklyCreatorInfo.generate();
 
-    test();
-
+    //ManageAccessToken.test();
   }
 
   /*
@@ -84,38 +86,6 @@ class _PageDevTestState extends State<PageDevTest> {
     });
   }
   */
-
-  void test() async
-  {
-    /*
-    var builder = new JWTBuilder();
-    var token = builder
-      ..issuer = 'https://api.foobar.com'
-      ..expiresAt = new DateTime.now().add(new Duration(minutes: 3))
-      ..setClaim('data', {'userId': 836})
-      ..getToken(); // returns token without signature
-
-    var signer = new JWTHmacSha256Signer('sharedSecret');
-    var signedToken = builder.getSignedToken(signer);
-    print(signedToken); // prints encoded JWT
-    var stringToken = signedToken.toString();
-    */
-
-    var signer = new JWTHmacSha256Signer('MIGTAgEAMBMGByqGSM49AgEGCCqGSM49AwEHBHkwdwIBAQQgFdWxe1+mSWMojsDjNpurrY20ZT6ZhuKKubjP+Pf1u5qgCgYIKoZIzj0DAQehRANCAASQrzJWzKzEfx7EKk9ZWgL6YShELIDH175JK75WXAmcsm8V6hQedvLNLeU8e3SblnplnetZyM9+Sg11Ug8dL+CH');
-
-    var decodedToken = new JWT.parse('eyJhbGciOiJFUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6IjM2RkxLODRUWkYifQ.eyJpYXQiOjE1NjYxOTI3MzYsImV4cCI6MTYyODQwMDc5NiwiaXNzIjoiWlo3Mk5DSjk0WSIsInVzZXJJZCI6IjExMzMifQ.iykzj3ML-JNXDQsx9Jw9ITbkMWEMlVDZw37JDOPcemGnQkF5pXfc-Zb5_FySdbkyet4zUuIUw0k7796KfDW3jQ');
-    // Verify signature:
-    print('DecodedTokenVerify :  ${decodedToken.verify(signer)}'); // true
-
-    /*
-    // Validate claims:
-    var validator = new JWTValidator() // uses DateTime.now() by default
-      ..issuer = 'https://api.foobar.com'; // set claims you wish to validate
-    Set<String> errors = validator.validate(decodedToken);
-    print(errors); // (empty list)
-    */
-  }
-
 
 
   Widget createTodayPopularComicInfoListView(BuildContext context, AsyncSnapshot snapshot)
